51aba3b2eefbc4c7757100692353e48e2ad3f4c9
Your Name <you@example.com>
2017-11-20 16:55:42 +0800

    test helloworld

--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "untitled.settings")
 
 application = get_wsgi_application()
+
+def hello(request):
+    return HttpResponse("Hello World")
